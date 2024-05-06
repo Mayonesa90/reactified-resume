1c1c711c19d35d7eb2791c88bb34121c974eb41b
Mayonesa90 <mayonaas@hotmail.com>
2024-05-06 10:45:55 +0200

    added font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+// import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
       <h1 className='text-lg text-pink-500'>This is App</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className=" font-inria-sans">mayaArzapaloBjörklund</h1>
+            <div>
+                <div></div>
+            </div>
+        </header>
+    )
+}
